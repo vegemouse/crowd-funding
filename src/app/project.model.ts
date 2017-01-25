dc2fc0514197e73f093f03410d7fda346c0efbdd
Ezra Sandzer-Bell and Max Cady <max@>
2017-01-25 08:58:04 -0800

    add model

--- src/app/project.model.ts
+++ src/app/project.model.ts
@@ -1,2 +1,3 @@
 export class Project {
+  constructor (public title: string, public manager: string, public teaser: string, public description: string, public moneyGoal: number, public moneyRaised: number, public benefits: string, public image: string, public city: string, public daysToGoal: number, public category: string) { }
 }
